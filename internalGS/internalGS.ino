8fcfd0bcd1f2c235f0905378a4bb5e5dd15dbfc4
Sviatoslav <94946672+MaterialI@users.noreply.github.com>
2023-09-11 16:51:03 -0700

    Solved issue on I2C of barometer and IMU data fetch
    Due to the rate of data coming to teency through I2C, barometer module couldn't send the data and issue I2C: 4 was arising. Solved, using threads.

--- internalGS/internalGS.ino
+++ internalGS/internalGS.ino
@@ -1,3 +1,5 @@
+#include <_Teensy.h>
+
 #include <Wire.h> //wire library to communicate
 #include <TeensyThreads.h> //threads 
 #include <PID_v1.h> //PID 
@@ -9,28 +11,156 @@ SFE_UBLOX_GNSS myGNSS; //defy a gps instance
 Adafruit_MPL3115A2 baro = Adafruit_MPL3115A2(); //defy a baro instance
 MPU9250 mpu; //gyro, accel, compass
 
+
+//threads 
+int accelThr;
+int baroThr;
+int gnssThr;
+//mutex locks definition
+
+Threads::Mutex I2CMutex;
+
+
+
 //GPS location
 int latitude;
-int longtitude;
+int longitude;
 float groundSpeed;
 
 //Gyroscope
-float roll;
-float pitch;
-float yaw;
+volatile float roll = 0.0;
+volatile float pitch = 0.0;
+volatile float yaw = 0.0;
+
+//Accelerometer
+volatile float accX =0.0;
+volatile float accY =0.0;
+volatile float accZ = 0.0;
+
+//Barometer
+float altitude = 0.0; //m
+float pressure = 0.0; //Pa
+float temperature_baro = 0.0; //Centigrade
 
 //PID coefficients
 float cP;
 float cI;
 float cD;
 
+void accelThread()
+{
+  while(1)
+  {
+    I2CMutex.lock();
+    if(mpu.update())
+    {
+      
+      yaw = mpu.getYaw();
+      roll = mpu.getRoll();
+      pitch = mpu.getPitch();
 
-void setup() {
-  // put your setup code here, to run once:
+      accX = mpu.getAccX();
+      accY = mpu.getAccY();
+      accZ = mpu.getAccZ();
 
+    }
+    I2CMutex.unlock();
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void baroThread()
+{
+  while(1)
+  {
+    I2CMutex.lock();
+    altitude = baro.getAltitude();
+    pressure = baro.getPressure();
+    temperature_baro = baro.getTemperature();
+    I2CMutex.unlock();
+  }
+}
 
+void gnssThread()
+{
+  while(1)
+  {
+    I2CMutex.lock();
+    latitude = myGNSS.getLatitude();
+    longitude = myGNSS.getLongitude();
+    groundSpeed= (float)(myGNSS.getGroundSpeed());
+    I2CMutex.unlock();
+    
+  }
+}
+
+
+void setup()
+{
+  
+  Serial.begin(9600); // Initialize the Serial Monitor
+
+  Wire.begin(); // Initialize I2C communication
+
+  if(baro.begin() == true)  //initialize barometer
+  {
+    Serial.println("Barometer module detected!");
+  }
+  else
+  {
+    Serial.println("Unable to detect Barometer module!");
+  }
+
+  if(mpu.setup(0x68) == true)
+  {
+    Serial.println("Accelerometer module detected!");
+    Serial.println("Calibrating compass, please wait...");
+    mpu.calibrateMag();
+    Serial.println("Calibration done!");
+    
+  }
+  else
+  {
+    Serial.println("Unable to detect Accelerometer module!");
+  }
+  if (myGNSS.begin() == true) //initialize GPS
+  {
+    Serial.println("GNSS module detected!");
+    // Configure GNSS module
+   // myGNSS.setUART1Output(COM_TYPE_UBX);
+    
+  }
+  else
+  {
+    Serial.println("Unable to detect GNSS module!");
+    //while (1);
+  }
+  //accelThr = threads.addThread(accelThread);
+  //baroThr = threads.addThread(baroThread);
+  gnssThr = threads.addThread(gnssThread);
+}
+
+
+
+void loop()
+{
+   
+      Serial.print("Heading (deg): ");
+      Serial.println(yaw);
+      // threads.wait(accelThr, 1000);
+      Serial.print("Baro Altitude (m): ");
+      Serial.println(altitude);
+      // Print latitude, longitude, and altitude
+      Serial.print("Latitude: ");
+      Serial.println(latitude);
+      Serial.print("Longitude: ");
+      Serial.println(longitude);
+      // Serial.print("Altitude (m): ");
+      // Serial.println(myGNSS.getAltitude()/1000);
+      
+      
+ 
+      
+      //Serial.println("---------------");
+    delay(1000);    
+  
 }
